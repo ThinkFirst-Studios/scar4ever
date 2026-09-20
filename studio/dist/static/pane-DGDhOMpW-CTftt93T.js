e7de524b8808412119455d0858832d697f108c15
thinkfirststudios <alex@thinkfirststudios.com>
2026-09-20 10:17:43 -0700

    Ignore studio build output

--- studio/dist/static/pane-DGDhOMpW-CTftt93T.js
+++
@@ -1,2 +0,0 @@
-import { i as PaneContainer } from "./sanity-Dwtw4qnl.js";
-export { PaneContainer as default };
--- studio/dist/static/pane-RxWH-K7v-yhWEQVqn.js
+++
@@ -1,2 +0,0 @@
-import { o as DocumentPane } from "./sanity-Dwtw4qnl.js";
-export { DocumentPane as default };
